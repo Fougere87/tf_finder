4ec7bbf97e0192d97269c6a0f077d8a7b58976d6
Fougere87 <chloe.mayere@gmail.com>
2016-12-14 13:17:20 +0100

    Fonctionnel... reste à paralleliser

--- feature_finder.py
+++ feature_finder.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
 
-import  pprint
 from BCBio.GFF import GFFExaminer
 from BCBio import GFF
-import os sys
-
-
-def get_features_from_gff(gff_file, li) :
-    """This function returns a dict object containing all the features
-    (seqRecord objects) selected by featType and chromosomes contained
-    in the provided GFF file. The chromosome list must be a list of strings"""
-    limite_info= dict( gff_id = chromosomes , gff_source = ['ensembl'], gff_type = [featType])
-    handle = open(gff_file)
-    gene_dict = {}
-    for rec in GFF.parse(handle, limit_info=limite_info) :
-        for feat in rec.features :
-            feat.qualifiers["chrom"] = rec.id
-            gene_dict[feat.qualifiers["gene_id"][0]] = feat
-    handle.close()
-    return gene_dict
+from Bio import SeqIO, motifs
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+from collections import Counter
+import os, sys, gc
 
 class FeatPosition:
     def __init__(self, id, chrom, start, end, strand):
@@ -28,43 +17,77 @@ class FeatPosition:
         self.end = end
         self.strand = strand
 
+def get_features_from_gff(gff_file, limite_info) :
+    """This function returns a dict object containing all the features
+    (as seqRecord objects) selected by featType and chromosomes contained
+    in the provided GFF file. The chromosome list must be a list of strings"""
+    handle = open(gff_file, 'r')
+    gene_dict = {}
+    for rec in GFF.parse(handle, limit_info=limite_info) :
+        for feat in rec.features :
+            feat.qualifiers["chrom"] = rec.id
+            gene_dict[feat.qualifiers["gene_id"][0]] = feat
+    handle.close()
+    return gene_dict
 
-def positions(gene_list, gene_dict) :
-    locations_dict = {}
-    for line in gene_list_file :
+def positions(gene_list_file, gene_dict) :
+    """Extract the position of the genes of interest from a gene dict"""
+    locations_list = []
+    gene_list  = open(gene_list_file, 'r')
+    for line in gene_list :
         try :
             feat = gene_dict[line[0:len(line)-1]]
             location = FeatPosition(id = line[0:len(line)-1],
                                     chrom = feat.qualifiers['chrom'],
                                     start = feat.location.nofuzzy_start,
                                     end = feat.location.nofuzzy_end,
-                                    strand = feat.location.strand  )
-            locations_dict[line[0:len(line)-1]] = location
-            #print(line[0:len(line)-1]+gene_dict[line[0:len(line)-1]].qualifiers['chrom']+gene_dict[line[0:len(line)-1]].location)
+                                    strand = feat.location.strand )
+            locations_list.append(location)
         except KeyError :
             print('Gene %s non trouvé', line[0:len(line)-1])
-    gene_list_file.close()
-    return locations_dict
+    gene_list.close()
+    return locations_list
 
-def findingSequences(fasta, features, distance) :
+def finding_sequences(fasta, features, distance=10000) :
+    """Extracts the ustream sequences from the gnenomic fasta file. Possible to set the distance from start"""
     genome_dict = SeqIO.to_dict(SeqIO.parse(fasta, "fasta"))
     sequences_list = []
     for feat in features :
-        if feat.strand = '+1' :
+        if feat.strand == '+1' :
             sequence  = genome_dict[feat.chrom].seq[(feat.start-1)-distance:(feat.start)]
+            sequence.alphabet= IUPAC.unambiguous_dna
             seqAnnot = SeqRecord(sequence)
             seqAnnot.id = feat.id
         else :
-            sequence  = genome_dict[feat.chrom].seq[(feat.start-1)-distance:(feat.start)].reverse_complement()
+            sequence  = genome_dict[feat.chrom].seq[(feat.end-1):(feat.end+distance-1)].reverse_complement()
+            sequence.alphabet= IUPAC.unambiguous_dna
             seqAnnot = SeqRecord(sequence)
             seqAnnot.id = feat.id
         sequences_list.append(seqAnnot)
+    del(genome_dict)
     return sequences_list
 
-in_file = "/home/mayere/Analyse_RNAseq/Genomes/Ensembl_Mmul86/Macaca_mulatta.Mmul_8.0.1.86.gtf"
-gene_list_file =open("/home/mayere/Analyse_RNAseq/Analysis/TF_discovery/TF_discov/gene_list.txt", 'r')
-limite_info= dict( gff_id = [str(i) for i in range(1,21)]+['X','Y'], gff_source = ['ensembl'], gff_type = ['gene'])
+def background(sequence) :
+    """Calculates the background of a sequence : the rate of ACTG  and returns it as a dictionnary"""
+    bckgrnd = {}
+    bckgrnd['A'] = sequence.count('A')/(len(sequence)-sequence.count('N'))
+    bckgrnd['C'] = sequence.count('C')/(len(sequence)-sequence.count('N'))
+    bckgrnd['G'] = sequence.count('G')/(len(sequence)-sequence.count('N'))
+    bckgrnd['T'] = sequence.count('T')/(len(sequence)-sequence.count('N'))
+    return(bckgrnd)
 
-get_features_from_gff(gff_file=in_file , limite_info=limite_info)
+def motifs_list(jasp_motifs_file) :
+    jasp_motifs = open(jasp_motifs_file, 'r')
+    motifs_list = []
+    for m in motifs.parse(jasp_motifs, "jaspar") :
+        motifs_list.append(m)
+    jasp_motifs.close()
+    return motifs_list
 
-print(gene_dict.keys())
+def align_motif(mot, sequence, background, precision = 10**4, balance = 1000, pseudocounts = 0.5) :
+    mot.background = background
+    mot.pseudocounts = pseudocounts
+    distribution = mot.pssm.distribution(background=background, precision=precision)
+    threshold = distribution.threshold_balanced(balance)
+    ret_list = [(position, score) for position, score in enumerate(mot.pssm.calculate(sequence)) if (position > 0)and(score > threshold)]
+    return ret_list
